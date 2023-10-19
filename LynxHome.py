677f5b6e5a932833eb6329ee75252b9ac8df0ba8
Matthew Rivera <rivera_matthew@lilly.com>
2023-10-19 13:46:59 -0700

    added more tabs for home page, added a home tab for each instrument

--- LynxHome.py
+++ LynxHome.py
@@ -8,13 +8,23 @@ from Widgets import Widgets as wg
 st.header("Lynx Home")
 database_filepath_alpha = "C:\\Matthew IC Copy For Test\\Database\\LillyLynxDatabase_Alpha_231018.db"
 database_filepath_bravo = "C:\\Matthew IC Copy For Test\\Database\\LillyLynxDatabase_231017.db"
-
 table_name = 'RunInfo'
 data_alpha = slhelp.load_data(slhelp, database_filepath=database_filepath_alpha, table_name=table_name)
 data_bravo = slhelp.load_data(slhelp, database_filepath=database_filepath_bravo, table_name=table_name)
 combined_data = pd.concat([data_alpha, data_bravo], axis=0)
 
+home_tab, mix_tab, norm_tab = st.tabs(['Home', 'DNA Mixing', 'Normalization'])
 
-wg.display_metrics_line(self=wg, data=combined_data, method_name=None)
-wg.display_runs_by_day(self=wg, data=combined_data, method_name=None)
+with home_tab:
+    wg.display_metrics_line(self=wg, data=combined_data, method_name=None)
+    wg.display_runs_by_day(self=wg, data=combined_data, method_name=None)
 
+with mix_tab:
+    method_name = "Biologica_DNA_Miniprep_Mixing_V3,0(MJR)"
+    wg.display_metrics_line(self=wg, data=combined_data, method_name=method_name)
+    wg.display_runs_by_day(self=wg, data=combined_data, method_name=method_name)
+
+with norm_tab:
+    method_name = "Biologica_Normalization_V3,0(MJR)"
+    wg.display_metrics_line(self=wg, data=combined_data, method_name=method_name)
+    wg.display_runs_by_day(self=wg, data=combined_data, method_name=method_name)
--- pages/Alpha.py
+++ pages/Alpha.py
@@ -1,22 +1,28 @@
 from StreamLitHelper import StreamLitHelper as slhelp
 import streamlit as st
 from Widgets import Widgets as wg
+import pandas as pd
 
 # streamlit run LynxAlpha.py
 
 st.header("Lynx - Alpha")
-tab1, tab2 = st.tabs(["DNA Mixing", "Normalization"])
 database_filepath = "C:\\Matthew IC Copy For Test\\Database\\LillyLynxDatabase_Alpha_231018.db"
 table_name = 'RunInfo'
 data = slhelp.load_data(slhelp, database_filepath=database_filepath, table_name=table_name)
 
 
-with tab1:
+home_tab, mix_tab, norm_tab = st.tabs(['Home', 'DNA Mixing', 'Normalization'])
+
+with home_tab:
+    wg.display_metrics_line(self=wg, data=data, method_name=None)
+    wg.display_runs_by_day(self=wg, data=data, method_name=None)
+
+with mix_tab:
     method_name = "Biologica_DNA_Miniprep_Mixing_V3,0(MJR)"
     wg.display_metrics_line(self=wg, data=data, method_name=method_name)
     wg.display_runs_by_day(self=wg, data=data, method_name=method_name)
 
-with tab2:
+with norm_tab:
     method_name = "Biologica_Normalization_V3,0(MJR)"
     wg.display_metrics_line(self=wg, data=data, method_name=method_name)
     wg.display_runs_by_day(self=wg, data=data, method_name=method_name)
